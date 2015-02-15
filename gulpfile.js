ff67db6f0e311ee8fe06be7b5fea9e31d4e463d2
selbekk <kristofer.selbekk@bekk.no>
2015-02-15 14:05:26 +0100

    Create a basic gulp file

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var del = require('del');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var plumber = require('gulp-plumber');
+var cssPrefixed = require('gulp-autoprefixer');
+var server = require('gulp-server-livereload');
+
+gulp.task('clear', function() {
+    del('static-web/assets/**', function(err) {
+        if(err) {
+            console.error('could not empty build folder. error:', err);
+            return;
+        }
+        console.log('emptied the static-web/assets folder.');
+    });
+});
+
+gulp.task('script', function() {
+    return gulp.src('static-web/js/*.js')
+        .pipe(plumber())
+        .pipe(jshint())
+        .pipe(uglify())
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('static-web/assets/'));
+
+});
+
+gulp.task('style', function() {
+    return gulp.src('static-web/css/*.css')
+        .pipe(cssPrefixed({browsers: ['last 2 versions'], cascade: false}))
+        .pipe(minifyCss())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('static-web/assets/'));
+});
+
+gulp.task('livereload', function() {
+    gulp.src('static-web')
+        .pipe(server({
+            livereload: true,
+            open: true,
+            log: 'debug'
+        }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('static-web/js/*.js', ['script']);
+    gulp.watch('static-web/css/*.css', ['style']);
+});
+
+
+gulp.task('build', ['clear', 'script', 'style']);
+gulp.task('serve', ['build', 'watch', 'livereload']);
+gulp.task('default', ['build']);
