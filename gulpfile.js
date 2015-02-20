8db83a230a9e2ced2d5cbd84edd2472145e3f27e
selbekk <kristofer.selbekk@bekk.no>
2015-02-21 00:43:51 +0100

    make clean work syncronously

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 // Install npm packages
 // Needs to be up here to work correctly
 gulp.task('install', function() {
-    gulp.src(['./package.json', './static-web/bower.json'])
+    gulp.src(['./package.json', './bower.json'])
         .pipe(install());
 });
 
@@ -18,52 +18,58 @@ var cssPrefixed = require('gulp-autoprefixer');
 var server = require('gulp-server-livereload');
 var install = require('gulp-install');
 var wiredep = require('wiredep').stream;
+var changed = require('gulp-changed');
 
 // Clean build folder
 gulp.task('clean', function() {
-    del('static-web/assets/**', function(err) {
-        if(err) {
-            console.error('could not empty build folder. error:', err);
-            return;
-        }
-        console.log('emptied the static-web/assets folder.');
-    });
+    var result = del.sync('dist/**');
+
+    if(!result) {
+        console.log('Project already cleaned. Skipping cleanup.');
+        return;
+    }
+    console.log('Deleted files/folders:\n', result.join('\n'));
 });
 
 // Handle frontend JS build
 gulp.task('script', function() {
-    return gulp.src('static-web/js/*.js')
+    return gulp.src('js/*.js')
         .pipe(plumber())
         .pipe(jshint())
         .pipe(uglify())
         .pipe(concat('scripts.min.js'))
-        .pipe(gulp.dest('static-web/assets/'));
+        .pipe(gulp.dest('dist/assets/'));
 
 });
 
 // Handle CSS build
 gulp.task('style', function() {
-    return gulp.src('static-web/css/*.css')
+    return gulp.src('css/*.css')
         .pipe(plumber())
         .pipe(cssPrefixed({browsers: ['last 2 versions'], cascade: false}))
         .pipe(minifyCss())
         .pipe(concat('styles.min.css'))
-        .pipe(gulp.dest('static-web/assets/'));
+        .pipe(gulp.dest('dist/assets/'));
 });
 
 // Wire in bower dependencies
 gulp.task('bower', function () {
-    gulp.src('./static-web/*.html')
+    gulp.src('*.html')
         .pipe(wiredep({
-            directory: './static-web/bower_components',
-            bowerJson: require('./static-web/bower.json')
+            // todo: prepend a / to path
         }))
-        .pipe(gulp.dest('./static-web/assets'));
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+   gulp.src('img/**')
+       .pipe(changed('dist/img'))
+       .pipe(gulp.dest('dist/img'));
 });
 
 // Development server @ localhost:8000
 gulp.task('server', function() {
-    gulp.src('static-web')
+    gulp.src('dist')
         .pipe(server({
             livereload: false,
             open: true,
@@ -74,12 +80,13 @@ gulp.task('server', function() {
 
 // Watch for updates in files to recompile assets
 gulp.task('watch', function() {
-    gulp.watch('static-web/js/*.js', ['script']);
-    gulp.watch('static-web/css/*.css', ['style']);
-    gulp.watch('static-web/bower.json', ['bower']);
+    gulp.watch('js/*.js', ['script']);
+    gulp.watch('css/*.css', ['style']);
+    gulp.watch('bower.json', ['bower']);
+    gulp.watch('img/*', ['images']);
 });
 
 
-gulp.task('build', ['clean', 'script', 'style', 'bower']);
+gulp.task('build', ['clean', 'script', 'style', 'bower', 'images']);
 gulp.task('serve', ['build', 'watch', 'server']);
 gulp.task('default', ['build']);
