838831b5a78cca25cd1e0aceb7ebd2ef92bf802f
selbekk <kristofer.selbekk@bekk.no>
2015-03-21 17:10:19 +0100

    Display the page title and content as fallback

--- theme/index.php
+++ theme/index.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
-<h1>Content</h1>
+    <div class="container">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <h2 class="page-title"><?php the_title(); ?></h2>
+            <div class="page-content">
+                <?php the_content(); ?>
+            </div>
+        <?php endwhile;
+        else : ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+    </div>
 
 <?php get_footer(); ?>
