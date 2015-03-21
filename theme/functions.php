c8942bae4551d1ca4d87d8ce5ebc92cfa623cf0a
selbekk <kristofer.selbekk@bekk.no>
2015-03-21 16:59:37 +0100

    Add support for cool features

--- theme/functions.php
+++ theme/functions.php
@@ -1,6 +1,32 @@
 <?php
-/* Utility functions go here */
+/* Theme setup */
 
+function theme_slug_render_title()
+{
+    ?>
+    <title><?php wp_title('|', true, 'right'); ?></title>
+<?php
+}
+
+add_action('wp_head', 'theme_slug_render_title');
+
+function register_theme_customizations()
+{
+    $html5args = array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+        'widgets'
+    );
+    add_theme_support('html5', $html5args);
+    add_theme_support('automatic-feed-links');
+    add_theme_support('title-tag');
+
+}
+
+add_action('after_setup_theme', 'register_theme_customizations');
 
 /* Navigation menu setup */
 
--- theme/header.php
+++ theme/header.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php wp_title('|', true, 'right'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
