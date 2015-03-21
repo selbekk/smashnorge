ab111b1d6d576f3c8c5e72f33c317d6dd025d8f3
selbekk <kristofer.selbekk@bekk.no>
2015-03-21 16:12:24 +0100

    Set up footer and menu

--- theme/functions.php
+++ theme/functions.php
@@ -1,2 +1,12 @@
 <?php
 /* Utility functions go here */
+
+
+/* Navigation menu setup */
+
+function register_main_menu()
+{
+    register_nav_menu('main-menu', __('Main Menu'));
+}
+
+add_action('init', 'register_main_menu');
