ebe15b43fd99bfac8d10af8200bb1a577906909c
selbekk <kristofer.selbekk@bekk.no>
2015-03-21 10:34:57 +0100

    add basic theme structure

---
+++ theme/footer.php
@@ -0,0 +1,37 @@
+
+
+    <div class="container-fluid footer-wrap">
+        <div class="container">
+            <footer id="main-footer" class="row">
+                <section class="col-sm-4 list contact-info">
+                    <h2>Smash Norge</h2>
+                    <ul>
+                        <li>Org. nr. xxx xxx xxx</li>
+                        <li>post@smashnorge.no</li>
+                        <li>Medlem av hyperion.</li>
+                    </ul>
+                </section>
+                <section class="col-sm-4 list social-media hidden-xs">
+                    <ul class="row no-header">
+                        <li class="col-xs-6"><a href="https://www.facebook.com/SmashNorge"><img
+                                    src="/assets/img/facebook-icon-white.png" alt="facebook"/></a></li>
+                        <li class="col-xs-6"><a href="https://twitter.com/SmashNorge"><img
+                                    src="/assets/img/twitter-icon-white.png" alt="twitter"/></a></li>
+                    </ul>
+                </section>
+                <section class="col-sm-4 list links hidden-xs">
+                    <h2>Lær mer</h2>
+                    <ul>
+                        <li><a href="#">En link</a></li>
+                        <li><a href="#">Enda en link</a></li>
+                        <li><a href="https://www.github.com/selbekk/smashnorge">Github</a></li>
+                    </ul>
+                </section>
+            </footer>
+        </div>
+    </div>
+
+    <script src="/assets/vendor.min.js"></script>
+    <script src="/assets/scripts.min.js"></script>
+</body>
+</html>
