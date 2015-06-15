93b965f6ba913532b7048c5498954f864bd307e2
n07028 <vegard.nyeng@nrk.no>
2015-06-15 10:38:29 +0200

    lagt til header.php

---
+++ src/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Smash Norge</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <link rel="stylesheet" href="/assets/vendor.min.css"/>
+    <link rel="stylesheet" href="/assets/styles.min.css" />
+</head>
+<body>
+    <div class="mobile-content visible-xs">
+        <!-- Mobile content (triggers, headers etc) -->
+        <div class="xs-trigger nav-trigger">
+            <span class="glyphicon glyphicon-menu-hamburger"></span>
+        </div>
+        <div class="xs-trigger search-trigger">
+            <span class="glyphicon glyphicon-search"></span>
+        </div>
+        <h1 class="main-title">Nyheter</h1>
+    </div>
+    <div class="container-fluid header-wrap bg bg-cover bg-panorama">
+        <div class="header-filter">
+            <nav id="main-nav" class="row">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-1 hidden-xs">
+                            <a href="/">
+                                <img src="/assets/img/small-logo-transparent-full.png" alt="Smash Norge logo"
+                                     class="nav-logo"/>
+                            </a>
+                        </div>
+                        <div class="col-sm-11">
+                            <ul class="nav nav-pills nav-justified">
+                                <li class="active"><a href="/news.html" title="">Nyheter</a></li>
+                                <li><a href="/signup.html" title="">Bli medlem</a></li>
+                                <li><a href="/tournaments.html" title="">Turneringer</a></li>
+                                <li><a href="/ranking.html" title="">Ranking</a></li>
+                                <li><a href="/about-us.html" title="">Om oss</a></li>
+                                <li><a href="/contact-us.html" title="">Kontakt oss</a></li>
+                                <li class="search-trigger"><a href="#" title="Søk"><span
+                                        class="glyphicon glyphicon-search hidden-xs"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
